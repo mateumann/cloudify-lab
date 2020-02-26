ef0b5d499da76b0a2fa1319ffd1efd5fff87b023
Mateusz Neumann <mateumann@gmail.com>
2020-02-26 10:17:09 +0100

    Use two VMs in bp_openstack_more

--- plugins/my_plugin/training.py
+++ plugins/my_plugin/training.py
@@ -8,4 +8,16 @@ def configure(ctx, **kwargs):
 
 @operation(resumable=True)
 def rel_postconfigure(ctx, **kwargs):
-    ctx.logger.info("Training rel_postconfigure ctx=%s kwargs=%s", ctx, kwargs)
+    if 'instance' in ctx:
+        ctx.logger.info("Post configure (training) for instance id=%s, runtime_properties=%s, " +
+                        "host_ip=%s, relationships=%s, kwargs=%s",
+                        ctx.instance.id, ctx.instance.runtime_properties, ctx.instance.host_ip,
+                        ctx.instance.relationships, kwargs)
+    elif 'node' in ctx:
+        ctx.logger.info("Post configure (training) for node id=%s, type_hierarchy=%s, " +
+                        "properties=%s, type=%s, kwargs=%s",
+                        ctx.node.id, ctx.node.type_hierarchy, ctx.node.properties, ctx.node.type,
+                        kwargs)
+    else:
+        ctx.logger.info("Post configure (training) for ctx=%s, kwargs=%s",
+                        ctx.__dict__, kwargs)
